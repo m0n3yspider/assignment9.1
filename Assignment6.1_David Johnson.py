42163b38c89ed2c9bb618bf7d0974cf61719e8c3
moneyspider813 <daj813@outlook.com>
2020-05-07 22:31:09 -0400

    Initial commit

---
+++ Assignment6.1_David Johnson.py
@@ -0,0 +1,18 @@
+#David Johnson 4-17-2020 Assignment 6.1
+
+#function to convert miles to kilometers
+def miTOkm(mi):
+    km = float(mi * 1.609)
+    return km
+
+#while loop to accept user input until valid
+while True:
+    try:
+        mi = float(input("Enter miles: "))
+        break
+    except:
+        print("Invalid input, please try again...")
+
+print("Total miles: ", mi)
+print("Total kilometers: ", miTOkm(mi))
+
